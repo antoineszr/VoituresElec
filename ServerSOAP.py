419171d4dda26cc3f6a491675d9eecffc3d29644
antoineszr <antoine.sauzer@gmail.com>
2021-11-25 11:59:44 +0100

    fin de seance

---
+++ ServerSOAP.py
@@ -0,0 +1,42 @@
+from spyne import *
+from spyne.server.wsgi import WsgiApplication
+from spyne.protocol.soap import Soap11
+from spyne import *
+from math import sin, cos, acos, pi
+import requests
+import json
+
+class tempsParcours(ServiceBase):
+    @rpc(String, String, String, String, String, _returns=String)
+    def tempsParcours(ctx, latA, longA, latB, longB, autonomie):
+        proto="http://wxs.ign.fr/essentiels/itineraire/rest/route.json?origin=" + latA + "," + longA + "&destination=" + latB + "," + longB + "&method=DISTANCE&graphName=Voiture"
+        rawdata= requests.get(proto)
+        json_loaded = rawdata.json()
+        duration = json_loaded.get('duration')
+        distance = json_loaded.get('distance')
+
+        print("distance = " + distance)
+        print("autaunomie = " + autonomie)
+
+        if distance < autonomie:
+            result = "Pas besoin de recharche, la durée du trajet est de " + duration
+        else:
+            distance = distance[:-3]
+            distanceINT = float(distance)
+            autonomieINT = float(autonomie)
+            restant = (distanceINT-autonomieINT)
+            temps = round(0.2*restant)
+            tempsSTR = str(temps)
+            result = ("L'autonomie n'est pas suffisante ! Le temps de trajet est de " + duration + " plus " + tempsSTR + " minutes de recharge")
+        return result
+
+            
+application = Application([tempsParcours], 'spyne.examples.hello.soap', in_protocol=Soap11(validator='lxml'), out_protocol=Soap11())
+wsgi_application = WsgiApplication(application)
+
+
+if __name__ == "__main__":
+
+    from wsgiref.simple_server import make_server
+    server = make_server('0.0.0.0', 80, wsgi_application)
+    server.serve_forever()
--- app.py
+++ app.py
@@ -1,11 +1,72 @@
-from sqlite3 import *
-from flask import Flask, render_template
-from . import db
-
-
+import sqlite3
+from flask import Flask
+from flask import render_template, request, redirect, url_for, g
 app = Flask(__name__)
-db.init_app(app)
+
+DATABASE = "database.db"
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def change_db(query,args=()):
+    cur = get_db().execute(query, args)
+    get_db().commit()
+    cur.close()
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    voitures_list=query_db("SELECT * FROM voitures")
+    return render_template("index.html",voitures_list=voitures_list)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+
+    if request.method == "GET":
+        return render_template("create.html",voitures=None)
+
+    if request.method == "POST":
+        voitures=request.form.to_dict()
+        values=[voitures["marque"],voitures["modele"],voitures["autonomie"]]
+        change_db("INSERT INTO voitures (marque,modele,autonomie) VALUES (?,?,?)",values)
+        return redirect(url_for("index"))
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def udpate(id):
+
+    if request.method == "GET":
+        voitures=query_db("SELECT * FROM voitures WHERE id=?",[id],one=True)
+        return render_template("update.html",voitures=voitures)
+
+    if request.method == "POST":
+        voitures=request.form.to_dict()
+        values=[voitures["marque"],voitures["modele"],voitures["autonomie"],id]
+        change_db("UPDATE voitures SET marque=?, modele=?, autonomie=? WHERE ID=?",values)
+        return redirect(url_for("index"))
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+
+    if request.method == "GET":
+        voitures=query_db("SELECT * FROM voitures WHERE id=?",[id],one=True)
+        return render_template("delete.html",voitures=voitures)
+
+    if request.method == "POST":
+        change_db("DELETE FROM voitures WHERE id = ?",[id])
+        return redirect(url_for("index"))
